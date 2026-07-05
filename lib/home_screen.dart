7fa7978fb8cfd837b3cb37802eeebea6fadecdbc
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-07-05 08:51:06 +0500

    Get Api Completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:rest_api/models/posts_model.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +12,89 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postsList = [];
+
+  Future<List<PostsModel>> getPostsApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postsList.add(PostsModel.fromJson(i));
+      }
+      return postsList;
+    }
+    return postsList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("REST API's")),
-      body: Column(),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostsApi(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: postsList.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 250,
+                        width: 250,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Card(
+                          color: Colors.pink,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                
+                                children: [
+                                  Text(
+                                    index.toString(),
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.start,
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                postsList[index].title.toString(),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                              Text(
+                                postsList[index].body.toString(),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                ),
+                                textAlign: TextAlign.start,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
