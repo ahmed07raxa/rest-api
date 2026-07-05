92ebba07fea727174e3f6513610522cc2e64546d
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-07-05 14:52:44 +0500

    Example Four

---
+++ lib/example_four.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ExampleFour extends StatefulWidget {
+  const ExampleFour({super.key});
+
+  @override
+  State<ExampleFour> createState() => _ExampleFourState();
+}
+
+class _ExampleFourState extends State<ExampleFour> {
+  var data;
+  Future<void> getUserApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/users'),
+    );
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("REST API's")),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getUserApi(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('Loading');
+                } else {
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Column(
+                          children: [
+                            Text(data[index]['name']),
+                            Text(data[index]['username']),
+                            Text(data[index]['email']),
+                            Text(data[index]['address']['geo']['lat']),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
